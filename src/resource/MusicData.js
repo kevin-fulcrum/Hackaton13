b2868e52fbaff49297366d237f3260200bc9515e
BMiguel <bmiguel@Mac-de-BMiguel.local>
2021-01-02 11:06:48 -0500

    Flatlist

--- src/resource/MusicData.js
+++ src/resource/MusicData.js
@@ -7,19 +7,19 @@ export const MusicData = [
     },
     {
         idImage: '2',
-        descripcion: 'Electronic',
+        title: 'Electronic',
         descripcion: 'Various Electronic music',
         url: 'https://cdn.icon-icons.com/icons2/439/PNG/512/Electronic_1_43111.png'
     },
     {
         idImage: '3',
-        descripcion: 'Classical',
+        title: 'Classical',
         descripcion: 'Various Classical music',
         url: 'https://cdn.icon-icons.com/icons2/439/PNG/512/Classical_2_43114.png'
     },
     {
         idImage: '4',
-        descripcion: 'Jazz',
+        title: 'Jazz',
         descripcion: 'Various Jazz music',
         url: 'https://cdn.icon-icons.com/icons2/439/PNG/512/Jazz_2_43102.png'
     },    
