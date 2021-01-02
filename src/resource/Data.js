c040b2325a5a303cfd060477d18339305d857585
Kevin <kevinariassistemas@gmail.com>
2021-01-02 09:22:28 -0500

    Data

---
+++ src/resource/Data.js
@@ -0,0 +1,52 @@
+export const Data = [
+    {
+        idImage: '1',
+        descripcion: 'Komedi',
+        url: 'https://cdn.icon-icons.com/icons2/2569/PNG/512/face_highlight_happy_icon_153759.png'
+    },
+    {
+        idImage: '2',
+        descripcion: 'Komedi',
+        url: 'https://cdn.icon-icons.com/icons2/1146/PNG/512/1486485552-338big-emoji-face-happy-smile-smiley_81151.png'
+    },
+    {
+        idImage: '3',
+        descripcion: 'Musik',
+        url: 'https://cdn.icon-icons.com/icons2/370/PNG/512/Music2_37175.png'
+    },
+    {
+        idImage: '4',
+        descripcion: 'Musik',
+        url: 'https://cdn.icon-icons.com/icons2/196/PNG/128/music_23795.png'
+    },
+    {
+        idImage: '5',
+        descripcion: 'Games',
+        url: 'https://cdn.icon-icons.com/icons2/1146/PNG/512/1486485585-aim-archery-focus-success-goal-objective-target_81181.png'
+    },
+    {
+        idImage: '6',
+        descripcion: 'Games',
+        url: 'https://cdn.icon-icons.com/icons2/370/PNG/512/Server3_37181.png'
+    },
+    {
+        idImage: '7',
+        descripcion: 'Games',
+        url: 'https://cdn.icon-icons.com/icons2/196/PNG/128/gamecontroller_23721.png'
+    },
+    {
+        idImage: '8',
+        descripcion: 'Berita',
+        url: 'https://cdn.icon-icons.com/icons2/196/PNG/128/news_23848.png'
+    },
+    {
+        idImage: '9',
+        descripcion: 'Berita',
+        url: 'https://cdn.icon-icons.com/icons2/1147/PNG/512/1486486291-article-news-newspaper-paper-rss_81203.png'
+    },
+    {
+        idImage: '9',
+        descripcion: 'Berita',
+        url: 'https://cdn.icon-icons.com/icons2/370/PNG/512/Sites3_37177.png'
+    },
+];
