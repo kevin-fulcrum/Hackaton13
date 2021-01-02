342c0c62709d9c5f49486ab9a69d89ace4d95d3a
kevin-fulcrum <kevinariassistemas@gmail.com>
2021-01-02 11:07:09 -0500

    []

---
+++ src/screens/Detalle.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, Text, StyleSheet, Image, FlatList, Animated} from 'react-native'
+
+const styles=StyleSheet.create({
+    
+});
+
+const Detalle =()=>{
+  return(
+    <>
+    
+    </>
+  )
+};
+
+export default Detalle;
